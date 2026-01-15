ea692a6db2d6d6de0caa3c48b67e46e9797a6333
Ahmet3457 <ahmetselimdurkut0@gmail.com>
2026-01-15 21:31:02 +0300

    Ornek Operatorler

---
+++ 260115_4_Ornek_Operatorler/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _260115_4_Ornek_Operatorler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool sonuc;
+
+
+            sonuc = 14 >= 14;//
+            //-----TRUE
+            Console.WriteLine("sonuc = 14 >= 14 sonuç" + sonuc);
+            sonuc = 14 >= 14 || (45 == 45);
+            Console.WriteLine("14 >= 14 || (45 == 45) sonuç:"+sonuc);
+            sonuc = 14=> 14 && (45 == 45 && "software" == "yazılım");
+            //--------true && (true && fasle)
+            //--------true && (false)
+            //--------true && false
+            //-------- false
+            Console.WriteLine("14>= 14 && (45 == 45 && \"software\"==\"yazılım\")sonuç:"+sonuc;
+            sonuc = 'a' != 'A' && (14 >= 4 || (DateTime.Now.Year == 2026 && true != false));//işlem sonucunu adım adım yazınız
+            sonuc = "Yaşar Kemal Van" != "Orhan Pamuk İstanbul" && ((14.5 == Convert.ToDouble("14.5") || false == true) && (double)45 == Convert.ToDouble("45"));//Adım adım çözümleri veriniz
+        }
+    }
+}
