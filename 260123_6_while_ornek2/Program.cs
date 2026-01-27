9af9878ae0c708077c05552658ea293b13b74eae
Ahmet3457 <ahmetselimdurkut0@gmail.com>
2026-01-27 20:50:21 +0300

    While Örnekleri

--- 260123_6_while_ornek2/Program.cs
+++ 260123_6_while_ornek2/Program.cs
@@ -10,6 +10,62 @@
             3) 500 den 0 a doğru 13 katlarını ekranda yazınız(tersten sayım olacak)
             4) 1-100 arasında asal sayıların toplamını hesaplayınız? (zor soru)
             */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            int sayi2 = 2;
+            //Bir sayı 1 ve kendisinden başka bölüneni yoksa
+
+
+
+            //1,2,3,...,10000
+            //1 asal değil???
+            //2 asaldır
+            //3=>2,3 yok=>değil
+            //4=>2,3 var=>4 değil
+            //5=>2,3,4 yok=> asaldır
+            //6=>2,3,4,5 var=> asal değil
+            //sayi2=11=>2,3,4,5,6,7,8,9,10
+            int bolenSayi = 2;
+            bool asalMi = true;
+
+            while(sayi2 > bolenSayi)
+            {
+                if(sayi2 % bolenSayi == 0)//eğer sayi2 bolenSayi ya tam bölünüyorsa bu sayi2nin asal olduğunu ispatlar
+                {
+                    asalMi = false;
+                    break;//içinde bulunduğu döngüyü sonlandırır.sayi2>bolenSayi koşullu döngüyü sonlandırır,diğer döngüye devam eder
+                }
+
+                bolenSayi++;
+
+            }
+            if (asalMi)
+            {
+                toplamAsalSayilar = toplamAsalSayilar + sayi2;
+                Console.Write(sayi2 + ",");
+            }
+            sayi2++;
+
         }
+        Console.WriteLine();
+        Console.WriteLine("Toplam Asal Sayılar:"+ toplamAsalSayilar);
     }
 }
---
+++ 260127_1_Super_sayi_While/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _260127_1_Super_sayi_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //kendisi hariç bölünenlerin toplamı kendisini veren sayılara süper sayı denir.1-100000 arasındaki süper sayıları listeleyiniz?
+            long sayi = 0;
+
+            while(sayi<100000)
+            {
+                long bolenSayi = 1;
+                long toplam = 0;
+                while (sayi > bolenSayi)
+                {
+                    if (sayi %bolenSayi==0)
+                    {
+                        toplam = toplam + bolenSayi;
+                    }
+                    bolenSayi++;
+                }
+                if(toplam == sayi)
+                {
+                    Console.WriteLine("Süper Sayı:" + sayi);
+                }
+                sayi++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
