3a0b71e6bfb44b2683dd4e75552e7dcd0be59acb
Ahmet3457 <ahmetselimdurkut0@gmail.com>
2026-01-29 20:20:08 +0300

    for dongusu

---
+++ 260129_2_for_ornek1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _260129_2_for_ornek1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //tek for ile 50-5000 arasındaki 11 ve 13 ün katları sayısı ve katları toplamlarını ayrı ayrı gösteriniz
+
+            int adet11 = 0, adet13 = 0, toplam11 = 0, toplam13 = 0;
+            
+            for(int i = 50; i<5000; i++)
+            {
+                if(i % 11 ==0)
+                {
+                    adet11++;
+                    toplam11 += i;
+                }
+                if(i % 13 ==0)
+                {
+                    adet13++;
+                    toplam13 += i;
+                }
+            }
+            Console.WriteLine("11 için sonuçlar:");
+            Console.WriteLine("11 sayısının katları:"+ adet11);
+            Console.WriteLine("11 sayısının katlarının toplamı:" + toplam11);
+
+            Console.WriteLine("----------------------------------------------");
+
+            Console.WriteLine("13 için sonuçlar:");
+            Console.WriteLine("13 sayısının katları:"+ adet13);
+            Console.WriteLine("13 sayısının katlarının toplamı:" + toplam13);
+
+
+        }
+    }
+}
---
+++ 260129_3_for_ornek2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _260129_3_for_ornek2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1-15000 arasındaki sayıların 5in katı olup, 2nin katı olmayan sayıların sayısı ve toplamlarını hesaplayınız
+
+            int toplam = 0, adet = 0;
+
+            for(int i =1; i <= 15000; i++)
+            {
+                if(i%5==0 && i%2!=0)
+                {
+                    adet++;
+                    toplam += i;
+                }
+
+            }
+            Console.WriteLine("1-15000 arasındaki 5in katı olup 2nin katı olmayan sayı adeti:{0} vetoplamları:{1}", adet, toplam);
+
+        }
+    }
+}
