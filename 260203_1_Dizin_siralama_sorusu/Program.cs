18620172e7fcd82b72542ebd3945bd7590d9e165
Ahmet3457 <ahmetselimdurkut0@gmail.com>
2026-02-05 20:30:50 +0300

    foreach

---
+++ 260203_1_Dizin_siralama_sorusu/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _260203_1_Dizin_siralama_sorusu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
