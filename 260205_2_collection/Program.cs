423a0b2c048a3882d2399448dfbb52826c58c741
Ahmet3457 <ahmetselimdurkut0@gmail.com>
2026-02-05 21:29:28 +0300

    Collection ornek1

---
+++ 260205_2_collection/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+
+namespace _260205_2_collection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Collection kullanmak için using System.Collections.ObjectModel; kütüphanesini en üst taraf olan using alanına eklenmesi gereklidir
+            Collection<string> list1 = new Collection<string>();//çalışanların isimleri
+            list1.Add("C#");
+
+
+            List<int> sayilar = new List<int>();//fabrika çalışan sırasını
+            sayilar.Add(10);
+
+            Collection<double> maaslar = new Collection<double>();
+            maaslar.Add(65125);
+            maaslar.Add(35125);
+            maaslar.Add(65100);
+            maaslar.Add(95450);
+            //foreach=> eleman sayısı belli olmayan Collection yapıları için listeleme yapar
+            foreach (var item in maaslar)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
